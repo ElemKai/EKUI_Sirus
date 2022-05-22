d1b2f2eedac690974775ddf072b72e6c00b2aa28
ElemKai <105996533+ElemKai@users.noreply.github.com>
2022-05-22 03:06:10 +0300

    6.095

--- ElvUI/Modules/UnitFrames/Units/Target_Sirus.lua
+++
@@ -1,7 +0,0 @@
-local E, L, V, P, G = unpack(ElvUI); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
-local UF = E:GetModule("UnitFrames")
-
-local frame = ElvUF_Target
-if not frame then return end
-
-frame.HeadHuntingWantedFrame = UF:Construct_HeadHuntingWanted(frame)
--- ElvUI_ProjectZidras/Modules/UnitFrames/Units/Player.lua
+++
@@ -1,9 +0,0 @@
-local PZ, T, E, L, V, P, G = unpack(select(2, ...))
-local ZUF = PZ.UnitFrames
-local UF = E.UnitFrames
-
-function ZUF:Construct_PlayerFrame(frame)
-	frame.HealCommBar = self:Construct_HealComm(frame)
-end
-
-hooksecurefunc(UF, "Construct_PlayerFrame", ZUF.Construct_PlayerFrame)
